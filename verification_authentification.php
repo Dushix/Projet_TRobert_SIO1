6f7a686c9399398502ddab6c3d7997fc8c83d719
Bader <krid.bader@gmail.com>
2022-12-08 21:24:13 +0100

    mise a jour de l auth

--- verification_authentification.php
+++ verification_authentification.php
@@ -1,103 +1,60 @@
 <?php
-foreach ($_POST as $key => $value) {
-    if ($key==="utilisateur") {
-        $utilisateur = $value;
-    }
-    if ($key==="password") {
-        $password = $value;
-    }
+if (isset($_POST['soumettre'])){
+    $utilisateur = (isset($_POST['identifiant'])) ? $_POST['identifiant'] : null;
+    $password = (isset($_POST['motDePasse'])) ? $_POST['motDePasse'] : null;
 }
+
+if(!empty($utilisateur)&&!empty($password)){
 
 // Verification de l utilisateur
 require ('./ConnectionMySQL.php') ;
-$sql = "select identifiant from comptes where identifiant= ?";
+$sql = "select identifiant, motDePasse from comptes where identifiant= :identifiant";
 $connection = getConnection();
-$verif = $connection->query($sql);
-$verif->bindParam(1, $utilisateur, PDO::PARAM_STR);
-$verif_id = $verif->fetchall();
-print_r($verif_id);
-$err_id = true;
-// foreach ($verif_id as $key1 => $value1) {
-//     foreach ($value1 as $key2 => $value2) {
-//         if ($key2 == "email"){
-//             if ($value2 == $nom) {
-//                 $err_id = false;
-//             }
-//         }
-//     }
-// }
+$instructions = $connection->prepare($sql);
+$instructions->bindParam(':identifiant', $utilisateur, PDO::PARAM_STR);
+$instructions->execute();
+$resultat = $instructions->fetchAll() ;
 
-// recuperation du mots de passe
-if ( $err_id == false) {
-
-    $sql = "select identifiant, motDePasse from comptes where identifiant= ?";
-    // ->bindParam(1, $utilisateur, PDO::PARAM_STR);
-    $connection = getConnection();
-    $verif = $connection->query($sql);
-    $verif_pasword = $verif->fetchall();
-
-    $hashDuMotDePasse = password_hash($password, PASSWORD_DEFAULT);
-    print_r($verif_pasword);
-    $err_pasword = true;
-<<<<<<< HEAD
-    // foreach ( $verif_pasword as $key => $value) {
-    //     foreach ( $value as $sousKey => $sousValue) {
-    //         if ($sousKey == "MotdePasse") {
-    //             if (password_verify(string $sousValue, string $hashDuMotDePasse) == TRUE) {
-    //                 $err_pasword = false;
-    //             }
-    //         }
-    //     }
-    // }
-=======
-    foreach ( $verif_pasword as $key => $value) {
-        foreach ( $value as $sousKey => $sousValue) {
-            if ($sousKey == "MotdePasse") {
-                if (password_verify(string $sousValue, string $hashDuMotDePasse) == TRUE) {
-                    $err_pasword = false;
-                }
-            }
-        }
-    }
->>>>>>> 41ad967ba86bd2045d0b35f603ba17af5412b9c5
-
-    if ( $err_pasword == false) {
-
-            echo("C'est Bon");
-
-        } else {
-            echo '<table>';
-            echo '<tr>';
-            echo '<td>email ou mots de passe incorrect</td>';
-            echo '</tr>';
-            echo '</table>';
-        }
-
-} else { 
+$code_err = 0;
+if ( $resultat == NULL) {
+    $code_err = 99;
     echo '<table>';
     echo '<tr>';
-    echo '<td>email ou mots de passe incorrect</td>';
+    echo '<td>identifiant ou mots de passe incorrect</td>';
     echo '</tr>';
     echo '</table>';
+} else {
+
+foreach ($resultat as $key => $value) {
+    foreach ($value as $sous_key => $sous_value) {
+        if ($sous_key == "motDePasse"){
+            
+            $hashDuMotDePasse = $sous_value;  
+        }
+    }
 }
+$pass_verif = password_verify($password, $hashDuMotDePasse);
 
-// $sql = "SELECT MotdePasse FROM prof";
-// $verif = $connection->query($sql);
-// $verif_pasword = $verif->fetchall();
+if ($pass_verif == true){
+    echo '<table>';
+    echo '<tr>';
+    echo '<td>Le mot de passe est valide</td>';
+    echo '</tr>';
+    echo '</table>';
+        } else {
+            $code_err = 99;
+            echo '<table>';
+            echo '<tr>';
+            echo '<td>identifiant ou mots de passe incorrect</td>';
+            echo '</tr>';
+            echo '</table>';
+            }
+    } 
 
-// $err_pasword = 1;
-// foreach ($verif_pasword as $key1 => $value1) {
-//     foreach ($value1 as $key2 => $value2) {
-//         if ($key2 === "MotdePasse"){
-//             if ($value2 === $password) {
-//                 $err_pasword = 0;
-//             }
-//         }
-//     }
-// }
-// if ($err_pasword == 0) {
-//     echo "bien pw";
-// } else { echo "NON pw";}
-
+}
+else {
+$code_err = 862;
+echo '<script type="text/javascript">alert("Vous devez remplir tous les champs"); </script>';
+}
 
 ?>
