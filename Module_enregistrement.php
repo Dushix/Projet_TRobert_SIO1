117f486d0b6f42c1f71ec07df02840c0b73390a4
BuildTools <unconfigured@null.spigotmc.org>
2022-12-30 01:50:15 +0100

    modification css de la page enregistrement

--- Module_enregistrement.php
+++ Module_enregistrement.php
@@ -96,34 +96,61 @@ if (isset($_GET['erreur'])){
                     }
 
             </script>-->
+        <form name="Formulaire" action="verification_enregistrement.php" method="post">
+            <div class="container">
+            <div class="message signup">
+                <div class="btn-wrapper">
+                <button class="button" id="signup">S'enregistrer</button>
+                <button class="button" id="login">Se connecter</button>
+                </div>
+            </div>
+            <div class="form form--signup">
+                <div class="form--heading">Bonjour! Enregistrez-vous</div>
+                <form autocomplete="off">
+                <input type="text" placeholder="Nom" name="nom" pattern="[a-zA-ZÀ-ÿ-.]{2,64}" title="Merci de fournir votre nom" required>
+                <input type="text" placeholder="Prenom" name="prenom" minlength="2" pattern="[a-zA-ZÀ-ÿ-.]{2,32}" title="Merci de fournir votre prenom" required>
+                <input type="email" placeholder="Email"  name="email"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Merci de fournir une adresse valide." required>
+                <input type="tel" placeholder="Telephone" name="telephone" maxlength="10" pattern="[0-9]{10}" required>
+                <input type="text" placeholder="Numen" name="numen" minlength="13" maxlength="13" pattern="[a-zA-0-9]{13}" required>
+                <input type="text" placeholder="Identifiant" name="identifiant" minlength="4" pattern="[a-zA-Z0-9]{4,16}"required>
+                <input type="password" placeholder="mot de passe" class="vide" name="password" minlength="8" required>
+                <input type="password" placeholder="Confirmation de mot de passe" class="vide" name="confirm_password" minlength="8" required>
+                <button class="button" name="verification" value="OK">S'enregistrer</button>
+                <input hidden type="submit" name="soumettre" value="OK"/>
 
-<div class="container">
-  <div class="message signup">
-    <div class="btn-wrapper">
-      <button class="button" id="signup">Sign Up</button>
-      <button class="button" id="login"> Login</button>
-    </div>
-  </div>
-  <div class="form form--signup">
-    <div class="form--heading">Welcome! Sign Up</div>
-    <form autocomplete="off">
-      <input type="text" placeholder="Name">
-      <input type="email" placeholder="Email">
-      <input type="password" placeholder="Password">
-      <button class="button">Sign Up</button>
-    </form>
-  </div>
-  <div class="form form--login">
-    <div class="form--heading">Welcome back! </div>
-    <form autocomplete="off">
-      <input type="text" placeholder="Name">
-      <input type="password" placeholder="Password">
-      <button class="button">Login</button>
-    </form>
-  </div>
-</div>
+                <script type="text/javascript">
+                    var pas = document.querySelector('[name="password"]');
+                    var con = document.querySelector('[name="confirm_password"]');
+                    var sub = document.querySelector('[type="submit"]');
+                    var but = document.querySelector('[type="button"]');
+
+                    but.addEventListener('click', testvf);
+
+                    function testvf() {
+                        if (pas.value === con.value ) {
+                            sub.click()
+                            // $('.button').prop('disabled', false);
+                            // document.forms["Formulaire"].submit();  
+                        } else {
+                            pas.id = "erreurpass";                  
+                            alert ("Votre mot de passe ne correspond pas avec la confirmation du Mot de passe");  
+                        }
+                    }
+                </script>
+                </form>
+            </div>
+            <div class="form form--login">
+                <div class="form--heading">Content de vous revoir! </div>
+                <form autocomplete="off">
+                <input type="text" placeholder="Name">
+                <input type="password" placeholder="Password">
+                <button class="button">Se connecter</button>
+                </form>
+            </div>
+            </div>
+        </form>
     </body>
 </html>
-<p>commit</p>
+
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="enregistrement.js"></script>
